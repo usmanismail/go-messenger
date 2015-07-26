4da8bffb8c28a0e8a4f360954321d531e2c44947
Usman Ismail <0xfffffff@gmail.com>
2015-07-25 23:58:47 -0400

    Adding cotainer for application

---
+++ go-auth/build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh 
+
+docker run --rm -it \
+    -v $PWD:/go/src/github.com/usmanismail/go-messenger/go-auth/ \
+    -e SOURCE_PATH=github.com/usmanismail/go-messenger/go-auth/ \
+    usman/go-builder:1.4
+
+docker build -t go-auth .

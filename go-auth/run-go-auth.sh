b56cb31ecc398823e2eb5b5ce3633e77bc305274
Usman Ismail <usman.ismail@kik.com>
2016-09-11 15:29:25 -0400

    Setting up for Kubernetes run

--- go-auth/run-go-auth.sh
+++ go-auth/run-go-auth.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
+
+# Add Entry for mysql to etc hosts
+echo "$GO_AUTH_MYSQL_SERVICE_HOST mysql" >> /etc/hosts
+
+# Create the database
+echo "create database messenger" | mysql -u root -p${MYSQL_ROOT_PASSWORD} -h mysql || echo "Already exists"
+
 ln -s /tmp/log /dev/log
 /bin/go-auth $@

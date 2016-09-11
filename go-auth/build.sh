bf4920dfde65af89c8315e3c420a5a6c3046fc69
Usman Ismail <usman.ismail@kik.com>
2016-09-11 19:02:36 -0400

    adding support for password file

--- go-auth/build.sh
+++ go-auth/build.sh
@@ -3,6 +3,6 @@
 docker run --rm -it \
     -v $PWD:/go/src/github.com/usmanismail/go-messenger/go-auth/ \
     -e SOURCE_PATH=github.com/usmanismail/go-messenger/go-auth/ \
-    usman/go-builder:1.4
+    usman/go-builder:1.4 bash
 
-docker build -t usman/go-auth .
+docker build -t usman/go-auth-kubernetes .
